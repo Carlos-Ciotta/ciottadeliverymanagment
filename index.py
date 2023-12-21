252390b20069c412a570fb4d701856c68604ee81
Carlos-Ciotta <carlos.ciotta2506@gmail.com>
2023-12-21 17:28:34 -0300

    commit

--- index.py
+++ index.py
@@ -3,6 +3,9 @@ from controllers import get_all, delete_by_id, put_status, post
 import pandas as pd
 from PIL import Image
 
+val = ['Em andamento', 'Aguardando']
+val1 = ['Entregue']
+val2= ['Aguardando']
 
 icon = Image.open('Logo.jpeg')
 st.set_page_config(
@@ -11,16 +14,34 @@ st.set_page_config(
     page_icon=icon,  # Você pode usar um emoji ou uma URL de uma imagem
 )
 
-val = ['Em andamento', 'Aguardando']
-val1 = ['Entregue']
-val2= ['Aguardando']
+def verifica_repetidos(dados_novos):
+    dados = get_all()
+    colunas = ["ID", "Nome Cliente", "Rua", "Bairro", "Telefone", "Status", "Hora", "Data", "Previsão de Entrega"]
+    data = pd.DataFrame(dados, columns=colunas)
+    df = data[data['Status'].isin(val)]
+
+    colunas_desejadas = ["Nome Cliente", "Rua", "Bairro"]
+    df_temp = df[colunas_desejadas]
+
+    indices_desejados = list(dados_novos.keys())[:3]
+    dados = {k: dados_novos[k] for k in indices_desejados}
+    # Cria uma condição que verifica se pelo menos uma linha atende à condição
+    condicao = (df_temp == dados).all(axis=1)
+
+    if condicao.any():
+        return True
+    else:
+        return False
+    
+
 def insere_entrega():
     nome_cliente = st.text_input("Nome cliente", key="nome_cliente_key")
     rua = st.text_input("Rua", key="rua_key")
     bairro = st.text_input("Bairro", key="bairro_key")
     telefone = st.text_input("Telefone", key="telefone_key")
-    previsao = st.text_input("Previsão de entrega", key="previsao_key")
-    if len(telefone)==0:
+    previsao = st.text_input("Previsao de Entrega", key = "previsao_key")
+
+    if (len(telefone)==0):
         telefone = "49911111111"
 
     if st.button("Enviar Entrega"):
@@ -36,8 +57,13 @@ def insere_entrega():
                 "data": "NULL",
                 "previsao":previsao
             }
-            response = post(data)
-            return response
+            aux = verifica_repetidos(data)
+            if aux:
+                response = post(data)
+                return response
+            else:
+                return st.write("Entrega já enviada")
+                
         else:
             return st.write("Campos de texto inválidos")
         
